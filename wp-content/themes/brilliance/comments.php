c9c1c28a8b725b86019d883bfd803a5437d51907
Mark Otto <mark.otto@twitter.com>
2011-04-06 08:19:35 -0700

    Clean up some comments code and styles

--- wp-content/themes/brilliance/comments.php
+++ wp-content/themes/brilliance/comments.php
@@ -5,9 +5,9 @@
         if (!empty($post->post_password)) { // if there's a password
             if ($_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) {  // and it doesn't match the cookie
 				?>
-				
+
 				<p class="nocomments">This post is password protected. Enter the password to view comments.<p>
-				
+
 				<?php
 				return;
             }
@@ -21,26 +21,28 @@
 
 <div id="comments">
 <?php if ($comments) : ?>
-	<h3><?php comments_number('No Responses', 'One Response', '% Responses' );?> to &#8220;<?php the_title(); ?>&#8221;</h3> 
+	<h3><?php comments_number('No Responses', 'One Response', '% Responses' );?> to &#8220;<?php the_title(); ?>&#8221;</h3>
 
 	<ol class="comment-list">
 
 	<?php foreach ($comments as $comment) : ?>
 
 		<li class="<?php echo $oddcomment; ?>" id="comment-<?php comment_ID() ?>">
-			<cite><?php comment_author_link() ?></cite> Says:
-			<?php if ($comment->comment_approved == '0') : ?>
-			<em>Your comment is awaiting moderation.</em>
-			<?php endif; ?>
-			<br />
-
-			<small class="commentmetadata"><a href="#comment-<?php comment_ID() ?>" title=""><?php comment_date('F jS, Y') ?> at <?php comment_time() ?></a> <?php edit_comment_link('e','',''); ?></small>
-
-			<?php comment_text() ?>
-
+		  <div class="author">
+		    <?php comment_author_link() ?>
+		  </div>
+		  <div class="content">
+  			<?php if ($comment->comment_approved == '0') : ?>
+    			<p class="awaiting-moderation">Your comment is awaiting moderation.</p>
+  			<?php endif; ?>
+  			<?php comment_text() ?>
+  			<small class="commentmetadata">
+  			 <a href="#comment-<?php comment_ID() ?>" title=""><?php comment_date('F jS, Y') ?> at <?php comment_time() ?></a> <?php edit_comment_link('e','',''); ?>
+  		  </small>
+		  </div>
 		</li>
 
-	<?php /* Changes every other comment to a different class */	
+	<?php /* Changes every other comment to a different class */
 		if ('alt' == $oddcomment) $oddcomment = '';
 		else $oddcomment = 'alt';
 	?>
@@ -51,13 +53,13 @@
 
  <?php else : // this is displayed if there are no comments so far ?>
 
-  <?php if ('open' == $post->comment_status) : ?> 
+  <?php if ('open' == $post->comment_status) : ?>
 		<!-- If comments are open, but there are no comments. -->
-		
+
 	 <?php else : // comments are closed ?>
 		<!-- If comments are closed. -->
 		<p class="nocomments">Comments are closed.</p>
-		
+
 	<?php endif; ?>
 <?php endif; ?>
 
