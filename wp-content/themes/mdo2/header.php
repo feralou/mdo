98f8a298edcb888da39dc3a3bfb5e2604382bfdd
Mark Otto <mark.otto@twitter.com>
2011-10-19 20:41:09 -0700

    adding ads

--- wp-content/themes/mdo2/header.php
+++ wp-content/themes/mdo2/header.php
@@ -65,3 +65,7 @@
         </a>
         <p>This is the website of one Mark Otto, a designer at Twitter.</p>
       </header>
+
+      <!-- InfluAds -->
+      <div id="influads_block" class="influads_block"></div>
+      <script type="text/javascript">(function(){ var acc = "acc_448d485_pub";var st ="nocss";var or= "v";var e=document.getElementsByTagName("script")[0];var d=document.createElement("script");d.src=('https:' == document.location.protocol ?'https://' : 'http://') +"engine.influads.com/show/"+or+"/"+st+"/"+acc;d.type="text/javascript"; d.async=true; d.defer=true; e.parentNode.insertBefore(d,e);})();</script>
